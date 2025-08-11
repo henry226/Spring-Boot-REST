7cd92900f833be817dbd6d4106df613e82fe13df
henry226 <henry226@bu.edu>
2025-08-10 22:56:43 -0400

    Dynamic filtering

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -2,20 +2,36 @@ package com.in28minutes.rest.webservices.restfulwebservices.filtering;
 
 import java.util.Arrays;
 import java.util.List;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FilteringController {
 
-  @GetMapping("/filtering")
-  public SomeBean filtering() {
-    return new SomeBean("value1", "value2", "value3");
-  }
+    @GetMapping("/filtering")
+    public MappingJacksonValue filtering() {
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field_one", "field_three");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        mappingJacksonValue.setFilters(filters);
+        return mappingJacksonValue;
+    }
 
-  @GetMapping("/filtering-list")
-  public List<SomeBean> filteringList() {
-    return Arrays.asList(
-        new SomeBean("value1", "value2", "value3"), new SomeBean("value4", "value5", "value6"));
-  }
+    @GetMapping("/filtering-list")
+    public MappingJacksonValue filteringList() {
+        List<SomeBean> someBeansList = Arrays.asList(
+                new SomeBean("value1", "value2", "value3"), new SomeBean("value4", "value5", "value6"));
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBeansList);
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field_one", "field_two");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        mappingJacksonValue.setFilters(filters);
+        return mappingJacksonValue;
+    }
 }
--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,58 +1,61 @@
 package com.in28minutes.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties("field_one")
+//@JsonIgnoreProperties("field_one")
 // Or @JsonIgnoreProperties({"field_one", "field_two"})
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
-  private String field_one;
+    private String field_one;
 
-  @JsonIgnore private String field_two;
-  private String field_three;
+    //@JsonIgnore
+    private String field_two;
+    private String field_three;
 
-  public SomeBean(String field_one, String field_two, String field_three) {
-    this.field_one = field_one;
-    this.field_two = field_two;
-    this.field_three = field_three;
-  }
+    public SomeBean(String field_one, String field_two, String field_three) {
+        this.field_one = field_one;
+        this.field_two = field_two;
+        this.field_three = field_three;
+    }
 
-  public String getField_one() {
-    return field_one;
-  }
+    public String getField_one() {
+        return field_one;
+    }
 
-  public void setField_one(String field_one) {
-    this.field_one = field_one;
-  }
+    public void setField_one(String field_one) {
+        this.field_one = field_one;
+    }
 
-  public String getField_two() {
-    return field_two;
-  }
+    public String getField_two() {
+        return field_two;
+    }
 
-  public void setField_two(String field_two) {
-    this.field_two = field_two;
-  }
+    public void setField_two(String field_two) {
+        this.field_two = field_two;
+    }
 
-  public String getField_three() {
-    return field_three;
-  }
+    public String getField_three() {
+        return field_three;
+    }
 
-  public void setField_three(String field_three) {
-    this.field_three = field_three;
-  }
+    public void setField_three(String field_three) {
+        this.field_three = field_three;
+    }
 
-  @Override
-  public String toString() {
-    return "SomeBean{"
-        + "field_one='"
-        + field_one
-        + '\''
-        + ", field_two='"
-        + field_two
-        + '\''
-        + ", field_three='"
-        + field_three
-        + '\''
-        + '}';
-  }
+    @Override
+    public String toString() {
+        return "SomeBean{"
+                + "field_one='"
+                + field_one
+                + '\''
+                + ", field_two='"
+                + field_two
+                + '\''
+                + ", field_three='"
+                + field_three
+                + '\''
+                + '}';
+    }
 }
